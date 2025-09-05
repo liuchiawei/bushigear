ef3deb39b808cbea0bf457e6f0053c1a1c02042e
LIU CHIAWEI <doublecheap@gmail.com>
2025-09-05 09:23:05 +0900

    feat/database: init migration

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,1 +1,10 @@
-// TODO: fetch data from database by Prisma
+import prisma from "@/lib/prisma";
+
+export async function GET() {
+  try {
+    const products = await prisma.product.findMany();
+    return Response.json(products);
+  } catch (error) {
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
