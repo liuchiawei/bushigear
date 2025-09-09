7c0d7adb776f041a8fa3ada5a16b1eec698e62eb
LIU CHIAWEI <doublecheap@gmail.com>
2025-09-09 12:39:20 +0900

    deploy: remove unused value

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -5,6 +5,6 @@ export async function GET() {
     const products = await prisma.product.findMany();
     return Response.json(products);
   } catch (error) {
-    return new Response("Internal Server Error", { status: 500 });
+    return new Response("Internal Server Error: " + error, { status: 500 });
   }
 }
