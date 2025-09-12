9e628b2ee77cee77b4c4c58919ccb18be7bc1a35
LIU CHIAWEI <doublecheap@gmail.com>
2025-09-12 12:54:09 +0900

    fix: unify product data variable

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -25,10 +25,9 @@ export default async function Products() {
         </h1>
       </div>
       <div className="w-full max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-        {productsData.map((product: Product) => (
+        {products.map((product: Product) => (
           <Grid key={product.id} product={product} />
         ))}
-      </div>
       </div>
     </div>
   );
